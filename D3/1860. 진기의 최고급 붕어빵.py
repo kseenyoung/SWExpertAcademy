a7393f8ea89a3c505a23498a3d7f982e2e2c853e
kseenyoung <kseenyoung@gmail.com>
2023-04-30 23:57:38 +0900

    ADD : D3 진기의 최고급 붕어빵(상세)

--- D3/1860. 진기의 최고급 붕어빵.py
+++ D3/1860. 진기의 최고급 붕어빵.py
@@ -1,17 +1,12 @@
 for test in range(1, int(input()) + 1):
     N, M, K = list(map(int, input().split()))
     times = list(map(int, input().split()))
-    times.sort()
-
-    prod = 0
-    time = 0
+    times.sort()  # 먼저 오는 손님 정렬
 
     for i in range(N):  # 아직 사람 있음
-        prod += (times[i]//M)*K
-        time = times[i]
-        if prod < i + 1:
+        prod = (times[i]//M)*K  # 해당 시간까지 만들 수 있는 붕어빵
+        if prod < i + 1:  # 몇 번째 손님
             print("#{} {}".format(test, "Impossible"))
             break
-        prod -= 1
     else:
         print("#{} {}".format(test, "Possible"))
